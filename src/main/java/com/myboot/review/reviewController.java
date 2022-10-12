54906b83fbac66163f1bcb763d0bd93914b93452
YangWooJin4266 <woo4266@naver.com>
2022-10-12 09:06:00 +0900

    리뷰 상세페이지 커밋입니다

--- src/main/java/com/myboot/review/reviewController.java
+++ src/main/java/com/myboot/review/reviewController.java
@@ -7,9 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public  class reviewController {
 
-	 @RequestMapping("/reviewboard.do")
+	 @RequestMapping("/reviewBoard.do")
 	  public String review(Model model){
 		System.out.println("리뷰");
-		return "reviewboard";
+		return "reviewBoard";
 	  }
+	 
+	 @RequestMapping("/reviewDetail.do")
+	  public String detail(Model model){
+		System.out.println("리뷰");
+		return "reviewDetail";
+	  }
+	 
 }
