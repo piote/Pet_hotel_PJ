138ce297d63a338a9f70a5577b0d0e5195586136
ddimonyang <114453387+ddimonyang@users.noreply.github.com>
2022-10-13 17:27:45 +0900

    문의게시판jsp업로드

--- src/main/java/com/myboot/questions/controller/questionsControllerImpl.java
+++ src/main/java/com/myboot/questions/controller/questionsControllerImpl.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class questionsControllerImpl {
+	
+	@RequestMapping("/questionsList.do")
+	public String questionsList(Model model){
+		System.out.println("q");
+		return "questionsList";
+	}
 	@RequestMapping("/questionsForm.do")
-	  public String review(Model model){
+	  public String questionsForm(Model model){
 		System.out.println("q");
 		return "questionsForm";
 	  }
