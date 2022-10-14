6eb590d2ec411e1ccd8b5d1096ff705196f5834e
piote <k21038108@gmail.com>
2022-10-14 13:37:41 +0900

    메인페이지 연결 /, /main.do
    main.do를 안치셔도 됩니다.

--- src/main/java/com/myboot/MainController.java
+++ src/main/java/com/myboot/MainController.java
@@ -7,16 +7,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public  class MainController {
-@ResponseBody
+
   @RequestMapping("/") 
   public String home(){
-    System.out.println("Hello Boot!!");
-    return "Hello Boot!!"; 
+    return "main"; 
   }
 
   @RequestMapping("/main.do")
   public String hello(Model model){
-	System.out.println("안녕하세요");
 	return "main";
   }
   @RequestMapping("/membership.do")
