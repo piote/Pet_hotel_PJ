78f0dd0e0a5dd8147c3a42a9a86ce78b4a36e53a
piote <k21038108@gmail.com>
2022-10-17 09:32:14 +0900

    리뷰 게시판 수정, properties 수정
    리뷰 게시판 대소문자 오류 수정.
    properties 구분을 위해 띄어쓰기

--- src/main/java/com/myboot/review/controller/ReviewControllerImpl.java
+++ src/main/java/com/myboot/review/controller/ReviewControllerImpl.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller("reviewController")
 public  class ReviewControllerImpl {
 
 	 @RequestMapping("/reviewBoard.do")
